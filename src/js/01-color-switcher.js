8b426ea44fb0f00ddd957ac08d8135ffe053e086
KolosIryna <kolos.irina23@gmail.com>
2023-08-08 13:22:56 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+  const body = document.querySelector("body");
+  const startBtn = document.querySelector("button[data-start]");
+  const stopBtn = document.querySelector("button[data-stop]");
+  let timerId = null;
 
+  startBtn.addEventListener("click", () => {
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  });
+  
+  
+  stopBtn.addEventListener("click", () => {
+    clearInterval(timerId);
+  });
+
+  function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
