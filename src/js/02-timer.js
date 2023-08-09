a1a7ea1f25abf5e77980ec539c5f84a93cfd1d91
KolosIryna <kolos.irina23@gmail.com>
2023-08-09 13:30:39 +0300

    task02

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,6 +1,19 @@
+// Описаний в документації
 import flatpickr from "flatpickr";
+// Додатковий імпорт стилів
 import "flatpickr/dist/flatpickr.min.css";
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  input: document.getElementById("datetime-picker"),
+  buttonStart: document.querySelector("button"),
+  days: document.querySelector("span[data-days]"),
+  hours: document.querySelector("span[data-hours]"),
+  minutes: document.querySelector("span[data-minutes]"),
+  seconds: document.querySelector("span[data-seconds]"),
+};
+let selectedDate;
 
 const options = {
     enableTime: true,
@@ -8,11 +21,36 @@ const options = {
     defaultDate: new Date(),
     minuteIncrement: 1,
     onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+      selectedDate = selectedDates[0];
+      if (selectedDate.getTime() <= new Date()) {
+        Notify.warning("Please choose a date in the future");
+        refs.buttonStart.setAttribute("disabled", "true");
+        return;
+      }
+      refs.buttonStart.removeAttribute("disabled");
+  },
+};
 
 flatpickr("#datetime-picker", options);
+
+refs.buttonStart.addEventListener("click", startTimer);
+function startTimer() {
+  const countdownInterval = setInterval(function () {
+    const now = new Date().getTime();
+    const timeRemaining =  selectedDate - now;
+
+    if (timeRemaining <= 0) {
+      clearInterval(countdownInterval);
+    } else {
+      const { days, hours, minutes, seconds } = convertMs(timeRemaining);
+
+      refs.days.textContent = days;
+      refs.hours.textContent = hours;
+      refs.minutes.textContent = minutes;
+      refs.seconds.textContent = seconds;
+    }
+  }, 1000);
+};
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
