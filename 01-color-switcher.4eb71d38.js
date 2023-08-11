1c4b0f99f54a71c5af1a128b42252db528fd136e
KolosIryna <127253667+KolosIryna@users.noreply.github.com>
2023-08-11 16:22:46 +0000

    Deploying to gh-pages from @ KolosIryna/goit-js-hw-09@bed01f504c9e2468464cd72ca96ab9ca42e7f312 🚀

--- 01-color-switcher.4eb71d38.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]"),o=null;e.addEventListener("click",(function(){o=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),n.addEventListener("click",(function(){clearInterval(o)}))}();
-//# sourceMappingURL=01-color-switcher.4eb71d38.js.map
